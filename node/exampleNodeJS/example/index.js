fa757282a48ac060a79b131a9c39417d160d151b
drahunpavel <drahunpavel@gmail.com>
2019-09-12 00:08:56 +0300

    lesson 15

--- node/exampleNodeJS/example/index.js
+++ node/exampleNodeJS/example/index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     if(req.method === 'GET'){
@@ -6,16 +8,35 @@ const server = http.createServer((req, res) => {
         //метод заголовка запроса
         //условие проверки заголовка
         res.writeHead(200, {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/html; charset=utf-8'
         })
 
-        res.end(`
-            <h1>Form</h1>
-                <form method='post' action='/'>
-                    <input name='title' type='text' />
-                    <button type='submit'>Send</button>
-                </form>
-        `)
+        //проверка страницы, которую нужно отдать
+        if(req.url === '/'){
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if(err){
+                        throw err;
+                    }else{
+                        res.end(content)
+                    }
+                }
+            )
+        }else if(req.url === '/about'){
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if(err){
+                        throw err;
+                    }else{
+                        res.end(content)
+                    }
+                }
+            ) 
+        };
     }else if(req.method === 'POST'){
 
         const body = [];
