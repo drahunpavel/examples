782b8fb5e6282dd5b7b30a73f8e497b308af46c5
drahunpavel <drahunpavel@gmail.com>
2019-09-11 23:56:16 +0300

    lesson 14

--- node/exampleNodeJS/example/index.js
+++ node/exampleNodeJS/example/index.js
@@ -1,21 +1,44 @@
-// const userObj = require('./user');
-// console.log('--userObj', userObj.user);
-// userObj.sayHi();
-
-// (function(require, modulem exports, __filename, __dirname){
-// })(); //так выглядит любой экспорт подуля под капотом
-
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.write('<h1>Hello from node js</h1>'); //отдаем на страницу
-    res.write('<h1>Hello from node js</h1>'); //отдаем на страницу
-    res.write('<h1>Hello from node js</h1>'); //отдаем на страницу
-    //res.end();
-    res.end(`<div style='background: red'>test</div>`); //зконец ответа
+    if(req.method === 'GET'){
+
+        //метод заголовка запроса
+        //условие проверки заголовка
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+
+        res.end(`
+            <h1>Form</h1>
+                <form method='post' action='/'>
+                    <input name='title' type='text' />
+                    <button type='submit'>Send</button>
+                </form>
+        `)
+    }else if(req.method === 'POST'){
+
+        const body = [];
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8' 
+        })
+
+        
+        req.on('data', data => {
+            body.push(Buffer.from(data));
+        })
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1];
+
+            res.end(`
+                <h1>Ваше сообщение: ${message}</h1>
+            `)
+        })
+    }
+
 });
 
 server.listen(3000, () => {
-    console.log('Server is starting')
+    console.log('Server is running...')
 })
