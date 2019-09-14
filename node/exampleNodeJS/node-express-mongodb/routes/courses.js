926bc9eb3d1cc825e39e168ddf875c169c63fabb
drahunpavel <drahunpavel@gmail.com>
2019-09-15 00:31:36 +0300

    lesson 39 update

--- node/exampleNodeJS/node-express-mongodb/routes/courses.js
+++ node/exampleNodeJS/node-express-mongodb/routes/courses.js
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get('/', async (req, res, next) => {
 
-    const courses = await Course.getAll();
+    const courses = await Course.find();
 
     res.render('courses', {
         title: 'All',
@@ -15,7 +15,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id/edit', async (req, res) => {
 
-    const course = await Course.getById(req.params.id)
+    const course = await Course.findById(req.params.id)
 
     if(!req.query.allow){
         return res.redirect('/'); //return ... функция прекращает выполнение и редиректится на главную страницу
@@ -29,7 +29,7 @@ router.get('/:id/edit', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     // console.log('--', req.params)
-    const course = await Course.getById(req.params.id)
+    const course = await Course.findById(req.params.id)
     res.render('course', {
         layout: 'empty',
         title: `Course ${course.title}`,
@@ -38,7 +38,9 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/edit', async (req, res) => {
-    await Course.update(req.body); //передаем на апдейт нужный курс
+    const { id} = req.body;
+    delete req.body.id;
+    await Course.findByIdAndUpdate(id, req.body); //передаем на апдейт нужный курс
     res.redirect('/courses');
 })
 
