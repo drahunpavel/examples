418f90bd00b3af038c58f6b8c6002d362cf8bcd0
drahunpavel <drahunpavel@gmail.com>
2020-05-30 22:48:12 +0300

    useContext

--- React Hooks/example2/src/TodoItem.js
+++ React Hooks/example2/src/TodoItem.js
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "./context";
 
 export default function TodoItem({ title, id, completed }) {
-  const [checked, setChecked] = useState(completed);
+  // const [checked, setChecked] = useState(completed);
+  const { removeTodo, toggleTodo } = useContext(Context); //получаем из контекста функции, которые передавали
 
   const cls = ["todo"];
 
-  if (checked) cls.push("completed");
+  if (completed) cls.push("completed");
 
   return (
     <li className={cls.join(" ")}>
       <label>
         <input
           type="checkbox"
-          checked={checked}
-          onChange={() => setChecked(!checked)}
+          checked={completed}
+          onChange={() => toggleTodo(id)}
         />
         <span>{title}</span>
 
-        <i className="material-icons red-text">delete</i>
+        <i className="material-icons red-text" onClick={() => removeTodo(id)}>
+          delete
+        </i>
       </label>
     </li>
   );
