04ff1d421f69dfd397ef656105eff7a6707e534f
drahunpavel <drahunpavel@gmail.com>
2020-06-01 08:59:39 +0300

    alert animation

--- React Hooks/my-app/src/components/Alert.js
+++ React Hooks/my-app/src/components/Alert.js
@@ -1,18 +1,37 @@
 import React, { useContext } from "react";
 import { AlertContext } from "../context/alert/alertContext";
+import { CSSTransition } from "react-transition-group";
 
 export const Alert = () => {
   const { alert, hide } = useContext(AlertContext); //получаем данные из контекста
 
-  if (!alert.visible) return null; //если alert нет, то компонент просто не рендерить
+  // if (!alert.visible) return null; //если alert нет, то компонент просто не рендерить
 
   return (
-    <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
-      <strong>Внимание</strong>
-      &nbsp;{alert.text}
-      <button onClick={hide} type="button" className="close" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+    <CSSTransition
+      in={alert.visible} //состояние для отображения нужного элемента
+      timeout={{
+        enter: 500,
+        exit: 350,
+      }}
+      classNames={"alert"}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div
+        className={`alert alert-${alert.type || "warning"} alert-dismissible`}
+      >
+        <strong>Внимание</strong>
+        &nbsp;{alert.text}
+        <button
+          onClick={hide}
+          type="button"
+          className="close"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
