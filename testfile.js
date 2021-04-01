e818b3842a861ffc2b988f2d78e773214d5e57fd
drahunpavel <drahunpavel@gmail.com>
2021-04-02 02:37:06 +0300

    test upd

--- testfile.js
+++ testfile.js
@@ -4,12 +4,13 @@
 // @version      1
 // @description  try to take over the world!
 // @author       You
+// @updateURL    https://raw.githubusercontent.com/drahunpavel/examples/master/testfile.js
 // @match        https://www.gwars.ru/wargroup.php*
 // @match        https://www.gwars.ru/b0*
 // @match        https://www.gwars.ru/warlog.php*
 // @match        https://www.gwars.ru/warlock.php*
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/drahunpavel/examples/master/testfile.js
+
 // ==/UserScript==
 
 
