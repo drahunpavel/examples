f53dea9b5c5178ceabf9430593fcbb4baa426b55
drahunpavel <drahunpavel@gmail.com>
2021-04-02 02:21:08 +0300

    upd

--- testfile.js
+++ testfile.js
@@ -1,3 +1,17 @@
+// ==UserScript==
+// @name         test Script
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.gwars.ru/wargroup.php*
+// @match        https://www.gwars.ru/b0*
+// @match        https://www.gwars.ru/warlog.php*
+// @match        https://www.gwars.ru/warlock.php*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/drahunpavel/user-scripts/main/testFile.js?token=AIW6AB7MHX54DRR4LWSEWDTAMZHY2
+// ==/UserScript==
+
 (function () {
     'use strict';
 
