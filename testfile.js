5fe80ba2b58be4a6866d389836abecb43bfefa0f
drahunpavel <drahunpavel@gmail.com>
2021-04-02 02:47:46 +0300

    upd

--- testfile.js
+++ testfile.js
@@ -4,7 +4,7 @@
 // @version      1.2.1
 // @description  try to take over the world!
 // @author       You
-// @updateURL    https://raw.githubusercontent.com/drahunpavel/examples/master/testfile.js
+// @updateURL    https://github.com/drahunpavel/examples/blob/master/testfile.js
 // @match        https://www.gwars.ru/wargroup.php*
 // @match        https://www.gwars.ru/b0*
 // @match        https://www.gwars.ru/warlog.php*
